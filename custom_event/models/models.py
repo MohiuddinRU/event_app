c2719341de9987bfc793309b83dee466e7ed3e2d
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-12-07 14:39:52 +0600

    event model and action

--- custom_event/models/models.py
+++ custom_event/models/models.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class custom_event(models.Model):
-#     _name = 'custom_event.custom_event'
-#     _description = 'custom_event.custom_event'
+class Events(models.Model):
+    _name = "event.event"
+    _description = "Events"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    title = fields.Char("Title")
+    location = fields.Char("Location")
+    date = fields.Datetime("Date")
