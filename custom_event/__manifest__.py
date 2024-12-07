39d7782d9376080f4483eb3d14ec42a34340d97b
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-12-07 15:41:09 +0600

    updated access

--- custom_event/__manifest__.py
+++ custom_event/__manifest__.py
@@ -11,7 +11,7 @@
     "version": "0.1",
     "depends": ["base"],
     "data": [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         "views/views.xml",
         "views/templates.xml",
     ],
