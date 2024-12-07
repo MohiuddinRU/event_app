e22459bcb173b7e371ff4b530bf0da30d3c0ecd1
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-12-07 15:40:46 +0600

    showing title as the display name

--- custom_event/models/models.py
+++ custom_event/models/models.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class Events(models.Model):
     _name = "event.event"
     _description = "Events"
+    _rec_name = "title"
 
     title = fields.Char("Title")
     location = fields.Char("Location")
