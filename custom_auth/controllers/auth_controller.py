33e021527c87d6f21c95a9ca87738353f29472ff
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-12-09 22:15:35 +0600

    updated the auth_controller to take value from request body

--- custom_auth/controllers/auth_controller.py
+++ custom_auth/controllers/auth_controller.py
@@ -2,25 +2,27 @@ import jwt
 import datetime
 from odoo import http
 from odoo.http import request, Response
+import json
 
-SECRET_KEY="secret_key"
+SECRET_KEY = "secret_key"
+
 
 class AuthController(http.Controller):
-    @http.route("/api/signin", type="json", auth="none", methods=["POST"])
+    @http.route("/api/signin", type="http", auth="public", methods=["POST"], csrf=False)
     def signin(self, **kwargs):
         try:
-            username = kwargs.get("username")
-            password = kwargs.get("password")
+            username = request.httprequest.json.get("username")
+            password = request.httprequest.json.get("password")
 
             if not username:
-                throw ValueError("Username required")
+                raise ValueError("Username required")
 
             if not password:
-                throw ValueError("Password required")
+                raise ValueError("Password required")
 
-            user = request.env["res.users"].sudo().search(["login", "=", username])
+            user = request.env["res.users"].sudo().search([("login", "=", username)])
 
-            if not user or not user._check_credentials(password):
+            if not user or not user._check_credentials(password, {'interactive': True}):
                 raise ValueError("Invalid username or password")
 
             payload = {
@@ -30,21 +32,19 @@ class AuthController(http.Controller):
                 "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
             }
 
-            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
-            return {
+            response =  {
                 "status": "error",
                 "message": "Authentication successful",
-                "token": token
+                "token": token,
             }
+            return json.dumps(response, default=str)
 
         except ValueError as e:
-            return {
-                "status": "error",
-                "message": str(e)
-            }
+            return json.dumps({"status": "error", "message": str(e)}, default=str)
         except Exception as e:
-            return {
+            return json.dumps({
                 "status": "error",
-                "message": "An unexpected error occurred: " + str(e)
-            }
+                "message": "An unexpected error occurred: " + str(e),
+            }, default=str)
