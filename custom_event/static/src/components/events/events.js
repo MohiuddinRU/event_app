a303151c02f7e5715213ebfa83e10252e3c8dd2a
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-12-09 19:37:30 +0600

    custom event owl component to show the events in snippet

--- custom_event/static/src/components/events/events.js
+++ custom_event/static/src/components/events/events.js
@@ -1,16 +1,27 @@
 /** @odoo-module */
-import {templates} from "@web/core/assets";
-import {Component, mount, whenReady} from "@odoo/owl";
+import { templates } from "@web/core/assets";
+import { Component, mount, whenReady, onWillStart, useState } from "@odoo/owl";
 
 export class Events extends Component {
     static template = "custom_event.events"
     setup() {
-        console.log('hello world')
+        onWillStart(this.onWillStart)
+        this.events = useState([])
+    }
+    async onWillStart() {
+        let response = await fetch('/api/events', {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+        },);
+        response = await response.json()
+        this.events = response.events
     }
 }
 
 whenReady(() => {
     let $selector = document.querySelector(".custom_events")
-    console.log("$selector = ", $selector)
-    mount(Events, $selector, {templates});
+    mount(Events, $selector, { templates });
 });
